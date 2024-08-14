5cdcb864eecf1b08ab935adc6f2bbad8bbcff7dc
YOUR NAME <YOUR EMAIL>
2024-08-14 01:31:28 -0500

    variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 
-const supabaseUrl = REACT_APP_SUPABASE_ANON_KEY;
-const supabaseAnonKey = REACT_APP_SUPABASE_URL;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 console.log('Supabase URL:', supabaseUrl);
 console.log('Supabase Anon Key:', supabaseAnonKey);
